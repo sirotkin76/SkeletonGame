6fc473b114c1707d2f3cd61c3343da9409aa0b3e
72gsir <72gsir@gmail.com>
2018-01-22 08:13:09 +0300

    Update menu

--- Assets/_script/menu/menu.cs
+++ Assets/_script/menu/menu.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class menu : MonoBehaviour {
 
+	void Start() {
+		Cursor.lockState = CursorLockMode.None;
+	}
+
 	public void LoadOneScene(){
 		Application.LoadLevel ("main");
 	}
